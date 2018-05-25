b7328dda06b7c0f4cae412effe5e2ef97bfd0893
Trey Dunn <trey.dunn90@gmail.com>
2018-05-25 13:59:30 -0400

    Add mock data for the employees data service.

---
+++ src/app/mock-employees.ts
@@ -0,0 +1,53 @@
+import { Employee } from './employee';
+export const EMPLOYEES: Employee[] = [
+  {
+    name: 'Mr. Nice',
+    title: 'Senior Analyst',
+    profileImage: 'https://randomuser.me/api/portraits/thumb/men/1.jpg'
+  },
+  {
+    name: 'Narco',
+    title: 'Analyst',
+    profileImage: 'https://randomuser.me/api/portraits/thumb/men/2.jpg'
+  },
+  {
+    name: 'Bombasto',
+    title: 'Analyst',
+    profileImage: 'https://randomuser.me/api/portraits/thumb/men/3.jpg'
+  },
+  {
+    name: 'Celeritas',
+    title: 'Senior Analyst',
+    profileImage: 'https://randomuser.me/api/portraits/thumb/women/1.jpg'
+  },
+  {
+    name: 'Magneta',
+    title: 'Analyst',
+    profileImage: 'https://randomuser.me/api/portraits/thumb/women/2.jpg'
+  },
+  {
+    name: 'RubberMan',
+    title: 'Analyst',
+    profileImage: 'https://randomuser.me/api/portraits/thumb/men/4.jpg'
+  },
+  {
+    name: 'Dynama',
+    title: 'Analyst',
+    profileImage: 'https://randomuser.me/api/portraits/thumb/women/3.jpg'
+  },
+  {
+    name: 'Dr IQ',
+    title: 'Senior Analyst',
+    profileImage: 'https://randomuser.me/api/portraits/thumb/men/5.jpg'
+  },
+  {
+    name: 'Magma',
+    title: 'Analyst',
+    profileImage: 'https://randomuser.me/api/portraits/thumb/women/4.jpg'
+  },
+  {
+    name: 'Tornado',
+    title: 'Senior Analyst',
+    profileImage: 'https://randomuser.me/api/portraits/thumb/men/6.jpg'
+  }
+];
