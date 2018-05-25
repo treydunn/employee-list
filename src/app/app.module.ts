8234a452cb8e0da4b12f9b0f554d2c21826b6567
Trey Dunn <trey.dunn90@gmail.com>
2018-05-25 14:21:47 -0400

    Add new employee service as a provider.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,9 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent }  from './app.component';
 
+import { EmployeeService } from './employee.service';
+
 @NgModule({
   imports:      [ BrowserModule ],
   declarations: [ AppComponent ],
-  bootstrap:    [ AppComponent ]
+  bootstrap:    [ AppComponent ],
+  providers: [ EmployeeService ]
 })
 export class AppModule { }
