3e657c04d90c11ead437801f1e6aa89fc685340d
Trey Dunn <trey.dunn90@gmail.com>
2018-05-25 14:22:32 -0400

    Update main app component for containing employee app components.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,39 @@
 import { Component } from '@angular/core';
+import { EmployeeService } from './employee.service';
+import { Employee } from './employee';
 
 @Component({
-  selector: 'my-app',
-  template: `<h1>Hello {{name}}</h1>`,
+  selector: 'employee-app',
+  templateUrl: './app.component.html'
 })
-export class AppComponent  { name = 'Angular'; }
+export class AppComponent  {
+
+  public title: string;
+
+  public employeeList: Employee[];
+
+  /**
+   * Creates an instance of AppComponent.
+   * @param {EmployeeService} employeeService 
+   * @constructor
+   */
+  constructor(private employeeService: EmployeeService) {
+    this.init();
+  }
+
+  /**
+   * Initializes component properties.
+   */
+  private init(): void {
+    this.title = 'Employee List';
+    this.getEmployees();
+  }
+
+  /**
+   * Gets employees through the employeeService
+   */
+  getEmployees(): void {
+    this.employeeList = this.employeeService.getEmployees();
+  }
+
+}
